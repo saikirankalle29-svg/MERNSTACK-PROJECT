efcf93f8b4ec33993ff1e4606b084abfc3861717
saikirankalle29-svg <saikirankalle29@gmail.com>
2026-08-11 23:28:02 +0530

    Connect frontend to Render backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://mernstack-project-6gel.onrender.com/api',
   headers: {
     'Content-Type': 'application/json'
   }
